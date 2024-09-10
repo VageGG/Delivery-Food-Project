eeeb708c70601dcfeb4e61908eb5f5404c3d1316
Vage Gevorgyan <Vage13@mail.ru>
2024-09-10 18:59:36 +0400

    entity menuItem

---
+++ src/main/java/com/fooddeliveryfinalproject/entity/MenuItem.java
@@ -0,0 +1,40 @@
+package com.fooddeliveryfinalproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "menu_items")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "menu_item_id")
+    private Long menuItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_category_id")
+    private MenuCategory menuCategory;
+
+    @OneToMany(mappedBy = "menuItem", cascade = CascadeType.ALL)
+    private List<CartItem> carts;
+
+    @OneToMany(mappedBy = "menuItem", cascade = CascadeType.ALL)
+    private List<OrderItem> orders;
+
+    private String name;
+
+    private Double price;
+
+    private String description;
+
+}
