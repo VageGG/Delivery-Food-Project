205318787cdc5e6c6244397320587a062765e77f
Vage Gevorgyan <Vage13@mail.ru>
2024-10-09 19:55:09 +0400

    Fixed name deliveryConverterTEst and validated annotation for userService

--- src/test/java/com/fooddeliveryfinalproject/converter/DeliveryConverterTest.java
+++ src/test/java/com/fooddeliveryfinalproject/converter/DeliveryConverterTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class DeliveryControllerTest {
+class DeliveryConverterTest {
 
     private DeliveryConverter converter;
 
